f09bd412e8ba93c46408d80f06e906d0960d4e5a
Muruganantham Pothanaickar <56486493+Murugananatham@users.noreply.github.com>
2024-01-06 08:24:50 +0530

    Add files via upload

---
+++ index_data_from_csv.py
@@ -0,0 +1,17 @@
+import csv
+
+file = open(f'C:\\Program Files\\Splunk\\etc\\apps\\flames_app\\bin\\sample_data.csv')
+type(file)
+
+csvreader = csv.reader(file)
+
+header = []
+header = next(csvreader)
+
+
+rows = []
+for row in csvreader:
+    print(row[0],";", row[1],";",row[2])
+
+file.close()
+
